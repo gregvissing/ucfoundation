a8666294c2cc172896371bab8a6eb97533d7b348
gregvissing <greg.vissing@gmail.com>
2020-11-02 07:59:44 -0500

    update styles for mixins

--- .eleventy.js
+++ .eleventy.js
@@ -14,68 +14,68 @@ const parseTransform = require('./src/transforms/parse-transform.js');
 // Import data files
 const site = require('./src/_data/site.json');
 
-module.exports = function(config) {
-  // Filters
-  config.addFilter('dateFilter', dateFilter);
-  config.addFilter('markdownFilter', markdownFilter);
-  config.addFilter('w3DateFilter', w3DateFilter);
+module.exports = function (config) {
+	// Filters
+	config.addFilter('dateFilter', dateFilter);
+	config.addFilter('markdownFilter', markdownFilter);
+	config.addFilter('w3DateFilter', w3DateFilter);
 
-  // Layout aliases
-  config.addLayoutAlias('home', 'layouts/home.njk');
+	// Layout aliases
+	config.addLayoutAlias('home', 'layouts/home.njk');
 
-  // Transforms
-  config.addTransform('htmlmin', htmlMinTransform);
-  config.addTransform('parse', parseTransform);
+	// Transforms
+	config.addTransform('htmlmin', htmlMinTransform);
+	config.addTransform('parse', parseTransform);
 
-  // Passthrough copy
-  config.addPassthroughCopy('src/fonts');
-  config.addPassthroughCopy('src/images');
-  config.addPassthroughCopy('src/js');
-  config.addPassthroughCopy('src/admin/config.yml');
-  config.addPassthroughCopy('src/admin/previews.js');
-  config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
-  config.addPassthroughCopy('src/robots.txt');
+	// Passthrough copy
+	config.addPassthroughCopy('src/fonts');
+	config.addPassthroughCopy('src/images');
+	config.addPassthroughCopy('src/js');
+	config.addPassthroughCopy('src/admin/config.yml');
+	config.addPassthroughCopy('src/admin/previews.js');
+	config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
+	config.addPassthroughCopy('src/robots.txt');
 
-  const now = new Date();
+	const now = new Date();
 
-  // Custom collections
-  const livePosts = post => post.date <= now && !post.data.draft;
-  config.addCollection('posts', collection => {
-    return [
-      ...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
-    ].reverse();
-  });
+	// Custom collections
+	const livePosts = post => post.date <= now && !post.data.draft;
+	config.addCollection('posts', collection => {
+		return [
+			...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)
+		].reverse();
+	});
 
-  config.addCollection('postFeed', collection => {
-    return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
-      .reverse()
-      .slice(0, site.maxPostsPerPage);
-  });
+	config.addCollection('postFeed', collection => {
+		return [...collection.getFilteredByGlob('./src/posts/*.md').filter(livePosts)]
+			.reverse()
+			.slice(0, site.maxPostsPerPage);
+	});
 
-  // Plugins
-  config.addPlugin(rssPlugin);
-  config.addPlugin(syntaxHighlight);
+	// Plugins
+	config.addPlugin(rssPlugin);
+	config.addPlugin(syntaxHighlight);
 
-  // 404
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('dist/404.html');
+	// 404
+	config.setBrowserSyncConfig({
+		callbacks: {
+			ready: function (err, browserSync) {
+				const content_404 = fs.readFileSync('dist/404.html');
 
-        browserSync.addMiddleware('*', (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      }
-    }
-  });
+				browserSync.addMiddleware('*', (req, res) => {
+					// Provides the 404 content without redirect.
+					res.write(content_404);
+					res.end();
+				});
+			}
+		}
+	});
 
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist'
-    },
-    passthroughFileCopy: true
-  };
+	return {
+		dir: {
+			input: 'src',
+			output: 'dist'
+		},
+		passthroughFileCopy: true
+	};
 };
